ab4431682d6fec58511d81e85d44636ea959e433
cenkh <1250578320@qq.com>
2015-04-01 20:56:56 +0800

    nothing

---
+++ CheckNetworking/CheckNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CheckNetworking
+//
+//  Created by cenkh on 15/4/1.
+//  Copyright (c) 2015年 cenkh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
