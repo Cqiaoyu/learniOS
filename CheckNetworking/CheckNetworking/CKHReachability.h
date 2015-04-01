97dac3e79e7fee753ea146b2fa2127d3e49a8d69
cenkh <1250578320@qq.com>
2015-04-01 21:59:39 +0800

    add reachability Class

---
+++ CheckNetworking/CheckNetworking/CKHReachability.h
@@ -0,0 +1,13 @@
+//
+//  CKHReachability.h
+//  CheckNetworking
+//
+//  Created by cenkh on 15/4/1.
+//  Copyright (c) 2015年 cenkh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CKHReachability : NSObject
+
+@end
---
+++ CheckNetworking/CheckNetworking/CKHReachability.m
@@ -0,0 +1,14 @@
+//
+//  CKHReachability.m
+//  CheckNetworking
+//
+//  Created by cenkh on 15/4/1.
+//  Copyright (c) 2015年 cenkh. All rights reserved.
+//
+
+#import "CKHReachability.h"
+
+@implementation CKHReachability
+
+
+@end
