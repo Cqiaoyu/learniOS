896774c70f46ef27a18c6ff8c41fd0d850d0e907
cenkh <1250578320@qq.com>
2015-04-04 12:43:54 +0800

    AutoLayoutDemo-01 begin AutoLayout

---
+++ LayoutDemo01/LayoutDemo01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LayoutDemo01
+//
+//  Created by cenkh on 15/4/4.
+//  Copyright (c) 2015年 cenkh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
